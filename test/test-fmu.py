77df662a20af639388814fa14774dabbe48618f0
Scheufler, Henning <henning.scheufler@dlr.de>
2021-11-06 11:28:48 +0100

    ENH: added assert statement in test

--- test/test-fmu.py
+++ test/test-fmu.py
@@ -6,6 +6,8 @@ model = load_fmu('../build/fmus/JavaSlave.fmu')
 
 
 res = model.simulate(final_time=1)
-print(res['someReal'])
+
+last_val = res['someReal'][-1]
+assert last_val <= 1 and last_val > 0.9
 
 
